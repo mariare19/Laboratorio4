3ff2221364e2358547f01e2c9faf32fb289765ed
Maria Reneé Palma <mariare_19@hotmail.com>
2018-09-26 18:21:20 -0600

    ejercicio 2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Holi');
+const x = 5;
+const y = 10;
+
+function sumaNumeros() {
+    return x + y;
+}
+
+function restaNumeros() {
+    let i = 10;
+    let j = 3;
+    return i - j;
+}
+
+function multiplicarNumeros(g, h) {
+    return g*h;
+}
+
+console.log("Suma de Numeros: " + sumaNumeros());
+console.log("Resta de Numeros: " + restaNumeros());
+console.log("Multiplicacion de Numeros: " + multiplicarNumeros(100, 2));
