b44df80c03fc956f213130d3dbc496ef771394f5
Maria Reneé Palma <mariare_19@hotmail.com>
2018-09-29 16:52:32 -0600

    validar cadena

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,14 +1,57 @@
-var index = 0;
+var indice = 0;
 
-function Validar(text) {
+function Validar(cadena) {
     try {
-        ValidarTexto(text);
-        console.log("Correcto");
+        ValidarTexto(cadena);
+        console.log("Cadena correcta");
     } catch (error) {
-        console.log(error + ", en la posicion: " + index);
+        console.log(error + ", en la posicion: " + (indice + 1));
     }
 }
 
-function siguienteCaracter(text, index) {
-    return text.substring(index, index + 1);
+function siguienteCaracter(cadena, indice) {
+    return cadena.substring(indice, indice + 1);
 }
+
+function ValidarTexto(cadena) {
+    let caracter = siguienteCaracter(cadena, indice);
+    switch (caracter) {
+        case "]":
+            break;
+        case "}":
+            break;
+        case ")":
+            break;
+        case "(":
+            indice = indice + 1;
+            ValidarTexto(cadena);
+            caracter = siguienteCaracter(cadena, indice);
+            if (caracter != ")") {
+                throw "Se espera )";
+            }
+            indice = indice + 1;
+            break;
+        case "{":
+            indice = indice + 1;
+            ValidarTexto(cadena);
+            caracter = siguienteCaracter(cadena, indice);
+            if (caracter != "}") {
+                throw "Se espera }";
+            }
+            indice = indice + 1;
+            break;
+        case "[":
+            indice = indice + 1;
+            ValidarTexto(cadena);
+            caracter = siguienteCaracter(cadena, indice);
+            if (caracter != "]") {
+                throw "Se espera ]";
+            }
+            indice = indice + 1;
+            break;
+        default:
+            break;
+    }
+}
+
+Validar("[{()}]");
