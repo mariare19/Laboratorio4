01bfcc7801f38796e4bfd00f2f6396b6d0a216f0
Maria Reneé Palma <mariare_19@hotmail.com>
2018-09-29 19:54:59 -0600

    cambios

--- ejercicio4.js
+++ ejercicio4.js
@@ -2,21 +2,12 @@ var http = require('http');
 var HttpDispatcher = require('httpdispatcher');
 var dispatcher = new HttpDispatcher();
 
-const port = 8080;
-
-function handleRequest(request, response) {
+var server = http.createServer(function (request, response) {
     try {
         dispatcher.dispatch(request, response);
     } catch (err) {
         console.log(err);
     }
-}
-
-var server = http.createServer(handleRequest);
-
-dispatcher.onGet("/", function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hey, this is the homepage of your server</h1>');
 });
 
 dispatcher.onGet("/hello", function (req, res) {
@@ -29,7 +20,7 @@ dispatcher.onGet("/hello", function (req, res) {
 
 dispatcher.onError(function (req, res) {
     res.writeHead(404);
-    res.end("Error, the URL doesn't exist");
+    res.end("Error, la url no existe!");
 });
 
-server.listen(port);
+server.listen(8080);
